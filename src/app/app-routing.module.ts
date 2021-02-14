b2130e4ed11049837143d2255d268b62c95f58a0
DevLabs <devlabs@MacBook-Pro-de-DevLabs.local>
2021-02-14 20:31:07 -0300

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CardListComponent } from './pages/card-list/card-list.component';
+import { CardEditComponent } from './pages/card-edit/card-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path: '', component: CardListComponent},
+	{path: 'establishments/:id', component: CardEditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxMaskModule, IConfig } from 'ngx-mask'
+
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
+
+import { WildcardContentComponent } from './components/wildcard-content/wildcard-content.component';
+import { CardComponent } from './components/card/card.component';
+import { CardFormComponent } from './components/card-form/card-form.component';
+
+import { CardEditComponent } from './pages/card-edit/card-edit.component';
+import { CardListComponent } from './pages/card-list/card-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WildcardContentComponent,
+    CardComponent,
+    CardFormComponent,
+    CardEditComponent,
+    CardListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgxMaskModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
